525beb7447c3877c55e8c810e1f9a7e56661eb0e
Mario David <mariodavid@arcor.de>
2015-11-14 20:22:35 +0100

    added seed data for products and product categories

---
+++ modules/core/db/init/hsql/30.create-db.sql
@@ -0,0 +1,42 @@
+insert into OM_PRODUCT_CATEGORY
+(NAME, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Tablet', 1, '2015-11-14 20:08:40', null, null, null, '13bcc20d-1975-1137-913b-be17e74de1e3', '2015-11-14 20:08:40', 'admin');
+
+insert into OM_PRODUCT_CATEGORY
+(NAME, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Not so smart phone', 1, '2015-11-14 20:08:55', null, null, null, '38ca386a-f23c-700b-e3fc-dbe2b1e0370f', '2015-11-14 20:08:55', 'admin');
+
+insert into OM_PRODUCT_CATEGORY
+(NAME, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Smartphone', 1, '2015-11-14 20:08:45', null, null, null, 'db45e357-64ab-5d6b-154f-86a8adc78e0f', '2015-11-14 20:08:45', 'admin');
+
+
+
+insert into OM_PRODUCT
+(NAME, CATEGORY_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Nokia 8810', '38ca386a-f23c-700b-e3fc-dbe2b1e0370f', 1, '2015-11-14 20:09:07', null, null, null, '43170403-a54b-6303-74e6-ed1ff36857b9', '2015-11-14 20:09:07', 'admin');
+
+insert into OM_PRODUCT
+(NAME, CATEGORY_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Apple iPad Air', '13bcc20d-1975-1137-913b-be17e74de1e3', 2, '2015-11-14 20:11:29', 'admin', null, null, '607c8f27-5b4a-15aa-4e5b-66efec5f4415', '2015-11-14 20:09:59', 'admin');
+
+insert into OM_PRODUCT
+(NAME, CATEGORY_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Samsung Galaxy Tab S2', '13bcc20d-1975-1137-913b-be17e74de1e3', 1, '2015-11-14 20:10:15', null, null, null, '888972d1-2883-6bf8-e4e8-88e71c5a589d', '2015-11-14 20:10:15', 'admin');
+
+insert into OM_PRODUCT
+(NAME, CATEGORY_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Samsung Galaxy S6', 'db45e357-64ab-5d6b-154f-86a8adc78e0f', 1, '2015-11-14 20:12:04', null, null, null, '88a71e28-4156-39b0-385e-d5a2d88ef73c', '2015-11-14 20:12:04', 'admin');
+
+insert into OM_PRODUCT
+(NAME, CATEGORY_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Apple iPhone 6', 'db45e357-64ab-5d6b-154f-86a8adc78e0f', 1, '2015-11-14 20:12:22', null, null, null, '926aabad-2a53-c7fa-dc23-9ce343cebc5b', '2015-11-14 20:12:22', 'admin');
+
+insert into OM_PRODUCT
+(NAME, CATEGORY_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Apple iPhone 6S', 'db45e357-64ab-5d6b-154f-86a8adc78e0f', 1, '2015-11-14 20:09:38', null, null, null, 'a022e550-b708-2e0d-7be8-b9f3ecadbd68', '2015-11-14 20:09:38', 'admin');
+
+insert into OM_PRODUCT
+(NAME, CATEGORY_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Nokia 6310', '38ca386a-f23c-700b-e3fc-dbe2b1e0370f', 1, '2015-11-14 20:09:19', null, null, null, 'adbfdf9a-a5c5-27b2-7479-fb7fbb04d3d5', '2015-11-14 20:09:19', 'admin');
+
