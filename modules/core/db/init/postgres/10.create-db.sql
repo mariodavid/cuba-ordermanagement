73d0a90a106789975210f893cfcfe7bee2ad92df
Mario David <mariodavid@arcor.de>
2016-05-02 09:08:20 +0200

    fixed docker files to work out with postgres

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -1,9 +1,9 @@
 -- begin OM_PRODUCT_CATEGORY
 create table OM_PRODUCT_CATEGORY (
-    ID varchar(36) not null,
+    ID uuid,
     CREATE_TS timestamp,
     CREATED_BY varchar(50),
-    VERSION integer,
+    VERSION integer not null,
     UPDATE_TS timestamp,
     UPDATED_BY varchar(50),
     DELETE_TS timestamp,
@@ -16,66 +16,67 @@ create table OM_PRODUCT_CATEGORY (
 -- end OM_PRODUCT_CATEGORY
 -- begin OM_PRODUCT
 create table OM_PRODUCT (
-    ID varchar(36) not null,
+    ID uuid,
     CREATE_TS timestamp,
     CREATED_BY varchar(50),
-    VERSION integer,
+    VERSION integer not null,
     UPDATE_TS timestamp,
     UPDATED_BY varchar(50),
     DELETE_TS timestamp,
     DELETED_BY varchar(50),
     --
     NAME varchar(255) not null,
-    CATEGORY_ID varchar(36) not null,
+    CATEGORY_ID uuid not null,
+    DESCRIPTION text,
     --
     primary key (ID)
 )^
 -- end OM_PRODUCT
 -- begin OM_LINE_ITEM
 create table OM_LINE_ITEM (
-    ID varchar(36) not null,
+    ID uuid,
     CREATE_TS timestamp,
     CREATED_BY varchar(50),
-    VERSION integer,
+    VERSION integer not null,
     UPDATE_TS timestamp,
     UPDATED_BY varchar(50),
     DELETE_TS timestamp,
     DELETED_BY varchar(50),
     --
     POSITION_ integer not null,
-    PRODUCT_ID varchar(36) not null,
+    PRODUCT_ID uuid not null,
     QUANTITY integer not null,
     QUANTITY_UNIT varchar(50) not null,
     UNIT_PRICE decimal(19, 2) not null,
     TOTAL_PRICE decimal(19, 2) not null,
-    ORDER_ID varchar(36) not null,
+    ORDER_ID uuid not null,
     --
     primary key (ID)
 )^
 -- end OM_LINE_ITEM
 -- begin OM_ORDER
 create table OM_ORDER (
-    ID varchar(36) not null,
+    ID uuid,
     CREATE_TS timestamp,
     CREATED_BY varchar(50),
-    VERSION integer,
+    VERSION integer not null,
     UPDATE_TS timestamp,
     UPDATED_BY varchar(50),
     DELETE_TS timestamp,
     DELETED_BY varchar(50),
     --
     ORDER_DATE date not null,
-    CUSTOMER_ID varchar(36) not null,
+    CUSTOMER_ID uuid not null,
     --
     primary key (ID)
 )^
 -- end OM_ORDER
 -- begin OM_CUSTOMER
 create table OM_CUSTOMER (
-    ID varchar(36) not null,
+    ID uuid,
     CREATE_TS timestamp,
     CREATED_BY varchar(50),
-    VERSION integer,
+    VERSION integer not null,
     UPDATE_TS timestamp,
     UPDATED_BY varchar(50),
     DELETE_TS timestamp,
@@ -90,3 +91,10 @@ create table OM_CUSTOMER (
     primary key (ID)
 )^
 -- end OM_CUSTOMER
+-- begin OM_PRODUCT_FILE_DESCRIPTOR_LINK
+create table OM_PRODUCT_FILE_DESCRIPTOR_LINK (
+    PRODUCT_ID uuid,
+    FILE_DESCRIPTOR_ID uuid,
+    primary key (PRODUCT_ID, FILE_DESCRIPTOR_ID)
+)^
+-- end OM_PRODUCT_FILE_DESCRIPTOR_LINK
